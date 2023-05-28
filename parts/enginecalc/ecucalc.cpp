7d07256de46dd10c0cc6e3816cfeaa91a38e956a
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2023-05-28 15:31:26 +0700

    Still confused with Engine Calculator

--- parts/enginecalc/ecucalc.cpp
+++ parts/enginecalc/ecucalc.cpp
@@ -17,6 +17,7 @@ ecu::ecu(QWidget *parent):
     connect(ui->actionAbout,SIGNAL(triggered()),this,SLOT(app_about()));
     connect(ui->actionQuit,SIGNAL(triggered()),this,SLOT(app_quit()));
     connect(ui->actionHelp,SIGNAL(triggered()),this,SLOT(app_help()));
+    connect(ui->actionCalc,SIGNAL(triggered()),this,SLOT(ecu_calc()));
 
     ui->txtRPM->setText("1000");
     ui->txtTPS->setText("200");
@@ -73,7 +74,60 @@ void ecu::ecu_view(){
 }
 
 void ecu::ecu_calc(){
+    int i;
+
     ecu_take();
+
+    rps=rpm/60;
+    rot_tick=10000/rps;
+    deg_rot_tick=rot_tick/360;
+
+    // Injection
+    for(i=0;i<5;i++){
+       if((tps>=data_tps[i])&&(tps<data_tps[i+1])){
+           inj_dur_deg=inj_data_dur_deg[i]+ ((inj_data_dur_deg[i+1]-inj_data_dur_deg[i])*(tps-data_tps[i])/(data_tps[i+1]-data_tps[i]));
+           inj_off_deg=inj_data_off_deg[i]+ ((inj_data_off_deg[i+1]-inj_data_off_deg[i])*(tps-data_tps[i])/(data_tps[i+1]-data_tps[i]));
+       }
+    };
+
+    if(tps<data_tps[0]){
+       inj_dur_deg=inj_data_dur_deg[0];
+       inj_off_deg=inj_data_off_deg[0];
+    }
+
+    if(tps>=data_tps[5]){
+       inj_dur_deg=inj_data_dur_deg[5];
+       inj_off_deg=inj_data_off_deg[5];
+    }
+
+    inj_on_deg=180-inj_dur_deg-inj_off_deg;
+
+    inj_dur_tick=inj_dur_deg*deg_rot_tick;
+    inj_on_tick =inj_on_deg*deg_rot_tick;
+
+    inj_vol= (float) flow_rate*inj_dur_tick/10000;
+
+
+    // Ignition
+    for(i=0;i<5;i++){
+       if((rpm>=data_rpm[i])&&(rpm<data_rpm[i+1])){
+           ign_off_deg=ign_data_off_deg[i]+ ((ign_data_off_deg[i+1]-ign_data_off_deg[i])*(rpm-data_rpm[i])/(data_rpm[i+1]-data_rpm[i]));
+       }
+    };
+
+    if(rpm<data_rpm[0]){
+       ign_off_deg=ign_data_off_deg[0];
+    }
+
+    if(rpm>=data_rpm[5]){
+       ign_off_deg=ign_data_off_deg[5];
+    }
+
+    ign_on_deg=180-ign_dur_deg-ign_off_deg;
+
+    ign_dur_tick=ign_dur_deg*deg_rot_tick;
+    ign_on_tick=ign_on_deg*deg_rot_tick;
+
     ecu_view();
     return;
 }
